76f39521bdc514bf3a929fa506b6c500245d4d8e
Francisco760 <f.arechiga@hotmail.com>
2018-07-31 22:55:44 -0600

    burger1

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers
+  (burgerName, devoured)
+VALUES
+  ("Texas Chainsaw Massa-Curd Burger", false),
+  ("Sweet Chili O’Mine Burger", false),
+  ("Bohemian Radishy Burger", false),
+  ("Top Bun Burger", false),
+  ("The Longest Chard Burger", false),
+  ("Gourdon-Hamsey Burger", false),
+  ("Not If I Can Kelp It Burger", true);
